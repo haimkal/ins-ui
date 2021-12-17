4183c8d86dda35d46599a6eb76d7e7dad145b258
Haim Kalvo <you@example.com>
2021-12-17 15:09:15 +0200

    linking from posts to users' profile

--- src/common/Post/Post.js
+++ src/common/Post/Post.js
@@ -12,36 +12,38 @@ export default function Post({ data }) {
     const { user } = useContext(UserContext)
 
     async function deletePost() {
-        history.push(`/profile/${user.username}`); 
+        history.push(`/profile/${user.username}`);
         return await PostService.deletePost(data._id);
     }
     return (
 
         <div className="col-12 col-md-4 ">
-                <article className="Post">
-                    <header>
+            <article className="Post">
+                <header>
+                    <Link to={`/profile/${data.user.username}`}>
                         <div className="Post__user">
                             <Avatar className="Post__user__avatar" size="sm" image={data.user.avatar} />
                             <h3 className="Post__user__username">{data.user.username}</h3>
                         </div>
-                        <div className="Post__date">
-                            <Moment fromNow className="Post__moment">{data.createdAt}</Moment>
-                        </div>
-                    </header>
-                    <div className="Post__image">
-                        <Link to={'/post/' + data._id}>
-                            <img className="Post__image" src={data.image} alt="dress" />
-                        </Link>
+                    </Link>
+                    <div className="Post__date">
+                        <Moment fromNow className="Post__moment">{data.createdAt}</Moment>
                     </div>
-                    <div className="Post__content">
-                        <p className="Post__description">{data.description}</p>
-                        <div className="Post__CurrentlyTakenBy">Currently taken by:<strong> {data.whereItIsNow}</strong></div>
-                    </div>
-                    <div className="d-flex justify-content-end">
-                        {data.user._id === user._id && <button className="deletePostButton" onClick={deletePost}> X </button>}
-                    </div>
-                </article>
-        </div>
+                </header>
+                <div className="Post__image">
+                    <Link to={'/post/' + data._id}>
+                        <img className="Post__image" src={data.image} alt="dress" />
+                    </Link>
+                </div>
+                <div className="Post__content">
+                    <p className="Post__description">{data.description}</p>
+                    <div className="Post__CurrentlyTakenBy">Currently taken by:<strong> {data.whereItIsNow}</strong></div>
+                </div>
+                <div className="d-flex justify-content-end">
+                    {data.user._id === user._id && <button className="deletePostButton" onClick={deletePost}> X </button>}
+                </div>
+            </article>
+        </div >
 
     )
 }
